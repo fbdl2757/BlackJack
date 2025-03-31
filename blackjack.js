44fdb37b150c594cdc92efc93fd0ea19397de87b
fbdl2757 <felicianolbd2757@aguafria.org>
2025-03-31 17:43:25 +0000

    message

--- blackjack.js
+++ blackjack.js
@@ -1,14 +1,14 @@
-let dealerCards = 0;
-let playerCards = 0;
-
 function getCards() {
     return Math.floor(math.random()*12);
 }
 
 function startGame() {
-    dealerCards = getCards() + getCards();
-    playerCards = getCards() + getCards();
-    console.log("you have a total of " + playerCards)
+    dealerCards1 = getCards();
+    dealerCards2 = getCards();
+    dealerCardsTotal = dealerCards1 + dealerCards2
+
+    let playerCards = getCards() + getCards();
+    console.log("you have a total of " + dealerCardsTotal)
 }
 
 startGame();
